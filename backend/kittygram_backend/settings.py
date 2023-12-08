899c4b37aa6ebb64e957b159798d1d6008824ee1
nickpydj <aeriexye@gmail.com>
2023-12-08 12:51:57 +0300

    add debug,allowed_hosts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
